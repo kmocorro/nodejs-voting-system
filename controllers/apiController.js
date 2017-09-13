065be55b8a23b4410129363435a611c4884542c9
vinx <kevinmocorro@gmail.com>
2017-09-13 23:04:00 +0800

    API rankings added and rankings engine

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,8 +1,41 @@
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var Promise = require('promise');
+var moment = require('moment');
+var fs = require('fs');
+var TSV = require('tsv');
+var md5 = require('md5');
 
 module.exports = function (app){
+
+    //  for date issues format
+    Date.prototype.toJSON = function() {
+        return moment(this).format("YYYY-MM-DD");
+    }
+
+    var today = new Date();
+    var dateAndtime = new Date();
+
+    let hh = today.getHours();
+    let min = today.getMinutes();
+    let sec = today.getSeconds();
+
+    let dd = today.getDate();
+    let mm = today.getMonth()+1; //January is 0!
+    let yyyy = today.getFullYear();
+
+    if(dd<10) {
+        dd = '0'+dd
+    } 
+
+    if(mm<10) {
+        mm = '0'+mm
+    } 
+
+    today = yyyy + '-' + mm + '-' + dd;
+    dateAndtime = yyyy + '-' + mm + '-' + dd + ' ' + hh + ':' + min + ':' + sec;
+
+    let date_time = moment(dateAndtime, "YYYY-MM-DD h:mm:ss");
 
     let poolLocal = mysql.createPool({
         connectionLimit:    100,
@@ -86,20 +119,28 @@ module.exports = function (app){
                         
                             if (obj.length > 0){
 
-                                if(obj[0].poster_id=='0'){
-                                    connection.query({
-                                        sql: 'UPDATE tbl_vote_values SET poster_id =?, vote_value = 1 WHERE employee_id = ?',
-                                        values: [req.body.poster_id, req.body.employee_id]
-                                    },  function(err, results, fields){
-                                        console.log(req.body.employee_id + 'has voted ' + req.body.poster_id +' !');
-                                    });
-                                    res.send('ok');
-                                }else{
-                                    res.send('Sorry, ' + obj[0].employee_id + ' has already voted');
+                                if(req.body.poster_id!='0'){
+
+                                    if(obj[0].poster_id=='0'){
+                                        connection.query({
+                                            sql: 'UPDATE tbl_vote_values SET poster_id =?, vote_value = 1, date_time = ? WHERE employee_id = ?',
+                                            values: [req.body.poster_id, dateAndtime, req.body.employee_id]
+                                        },  function(err, results, fields){
+                                            
+                                        });
+                                        res.send('ok');
+                                        console.log(req.body.employee_id + ' has voted ' + req.body.poster_id +' at ' + date_time + '!');
+                                    }else{
+                                        res.send('Sorry, ' + obj[0].employee_id + ' has already voted');
+                                    }
+
+                                } else {
+                                    res.send('poster number does not exist');
                                 }
                                 
+                                
                             } else {
-                                res.send('Id number does not exist');
+                                res.send('id number does not exist');
                             }
         
                     });
@@ -110,6 +151,39 @@ module.exports = function (app){
         
                 }
             
+        });
+
+    });
+
+    app.get('/rankings', function(req, res){
+        
+        poolLocal.getConnection(function(err, connection){
+
+            connection.query({
+               sql: 'SELECT date_time, poster_id, SUM(vote_value) AS total_votes  FROM tbl_vote_values  WHERE  date_time >= CONCAT("2017-09-10"," 06:30:00")  GROUP BY  poster_id   ORDER BY  SUM(vote_value) DESC'
+            },  function(err, results, fields){
+
+                let obj = [];
+
+                    for(let i=0; i < results.length; i++){
+                        obj.push({
+                            poster_id: md5(results[i].poster_id + 718),
+                            total_votes: results[i].total_votes
+                        });
+                    }
+                    
+                var json_obj = JSON.stringify(obj);
+                var tsv_obj = TSV.stringify(obj);
+                
+                fs.writeFile('./public/rankings.tsv', tsv_obj, function(err){
+                    if(err) throw err;
+                    console.log('done writefile' +tsv_obj);
+                });
+                
+                res.render('rankings');
+                
+            });
+
         });
 
     });
---
+++ public/rankings.js
@@ -0,0 +1,92 @@
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+width = 960 - margin.left - margin.right,
+height = 500 - margin.top - margin.bottom;
+
+var formatPercent = d3.format("");
+
+var x = d3.scale.ordinal()
+.rangeRoundBands([0, width], .1, 1);
+
+var y = d3.scale.linear()
+.range([height, 0]);
+
+var xAxis = d3.svg.axis()
+.scale(x)
+.orient("bottom");
+
+var yAxis = d3.svg.axis()
+.scale(y)
+.orient("left")
+.tickFormat(formatPercent);
+
+var svg = d3.select("body").append("svg")
+.attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.tsv("./rankings.tsv", function(error, data) {
+
+data.forEach(function(d) {
+d.total_votes = +d.total_votes;
+});
+
+x.domain(data.map(function(d) { return d.poster_id; }));
+y.domain([0, d3.max(data, function(d) { return d.total_votes; })]);
+
+svg.append("g")
+  .attr("class", "x axis")
+  .attr("transform", "translate(0," + height + ")")
+  .call(xAxis);
+
+svg.append("g")
+  .attr("class", "y axis")
+  .call(yAxis)
+.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 6)
+  .attr("dy", ".71em")
+  .style("text-anchor", "end")
+  .text("total_votes");
+
+svg.selectAll(".bar")
+  .data(data)
+.enter().append("rect")
+  .attr("class", "bar")
+  .attr("x", function(d) { return x(d.poster_id); })
+  .attr("width", x.rangeBand())
+  .attr("y", function(d) { return y(d.total_votes); })
+  .attr("height", function(d) { return height - y(d.total_votes); });
+
+d3.select("input").on("change", change);
+
+var sortTimeout = setTimeout(function() {
+d3.select("input").property("checked", true).each(change);
+}, 2000);
+
+function change() {
+clearTimeout(sortTimeout);
+
+// Copy-on-write since tweens are evaluated after a delay.
+var x0 = x.domain(data.sort(this.checked
+    ? function(a, b) { return b.total_votes - a.total_votes; }
+    : function(a, b) { return d3.ascending(a.poster_id, b.poster_id); })
+    .map(function(d) { return d.poster_id; }))
+    .copy();
+
+svg.selectAll(".bar")
+    .sort(function(a, b) { return x0(a.poster_id) - x0(b.poster_id); });
+
+var transition = svg.transition().duration(750),
+    delay = function(d, i) { return i * 50; };
+
+transition.selectAll(".bar")
+    .delay(delay)
+    .attr("x", function(d) { return x0(d.poster_id); });
+
+transition.select(".x.axis")
+    .call(xAxis)
+  .selectAll("g")
+    .delay(delay);
+}
+});
